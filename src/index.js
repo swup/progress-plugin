7e1efe77e234825ae0148946eebc17523c2bc26e
Rasso Hilber <mail@rassohilber.com>
2022-03-07 13:42:31 +0100

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -3,71 +3,70 @@ import ProgressBar from './ProgressBar';
 
 export default class SwupProgressPlugin extends Plugin {
 	name = 'SwupProgressPlugin';
-
+	
 	constructor(options) {
 		super();
 		const defaultOptions = {
 			className: 'swup-progress-bar',
 			transition: 300,
 			delay: 300,
-      hideImmediately: true
+			hideImmediately: true
 		};
-
+		
 		this.options = {
 			...defaultOptions,
 			...options
 		};
-
+		
 		this.showProgressBarTimeout = null;
 		this.progressBar = new ProgressBar({
 			className: this.options.className,
 			animationDuration: this.options.transition
 		});
 	}
-
+	
 	mount() {
 		this.swup.on('transitionStart', this.startShowingProgress);
 		this.swup.on('contentReplaced', this.stopShowingProgress);
 	}
-
+	
 	unmount() {
 		this.swup.off('transitionStart', this.startShowingProgress);
 		this.swup.off('contentReplaced', this.stopShowingProgress);
 	}
-
+	
 	startShowingProgress = () => {
 		this.progressBar.setValue(0);
 		this.showProgressBarAfterDelay();
 	};
-
+	
 	stopShowingProgress = () => {
 		this.progressBar.setValue(1);
-    if( this.options.hideImmediately ) {
-      this.hideProgressBar();
-    } else {
-      this.finishAnimationAndHideProgressBar();
-    }
-		
+		if( this.options.hideImmediately ) {
+			this.hideProgressBar();
+		} else {
+			this.finishAnimationAndHideProgressBar();
+		}
 	};
-
+	
 	showProgressBar = () => {
-    if (this.hideProgressBarTimeout != null) {
-      window.clearTimeout(this.hideProgressBarTimeout);
-      delete this.hideProgressBarTimeout;
-    }
+		if (this.hideProgressBarTimeout != null) {
+			window.clearTimeout(this.hideProgressBarTimeout);
+			delete this.hideProgressBarTimeout;
+		}
 		this.progressBar.show();
 	};
-
+	
 	showProgressBarAfterDelay = () => {
 		this.showProgressBarTimeout = window.setTimeout(this.showProgressBar, this.options.delay);
 	};
-
-  hideProgressBar = () => {
-    this.progressBar.hide();
-  };
-
+	
+	hideProgressBar = () => {
+		this.progressBar.hide();
+	};
+	
 	finishAnimationAndHideProgressBar = () => {
-    this.hideProgressBarTimeout = window.setTimeout(this.hideProgressBar, this.options.transition);
+		this.hideProgressBarTimeout = window.setTimeout(this.hideProgressBar, this.options.transition);
 		
 		if (this.showProgressBarTimeout != null) {
 			window.clearTimeout(this.showProgressBarTimeout);
