ed97025d8a343f8a18cd7f09c5d03a3a52582217
Philipp Daun <post@philippdaun.net>
2022-07-20 16:03:41 +0200

    Remove progressbar after transition

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -90,7 +90,7 @@ export default class ProgressBar {
 
 	uninstallProgressElement() {
 		if (this.progressElement.parentNode) {
-			// document.documentElement.removeChild(this.progressElement);
+			document.documentElement.removeChild(this.progressElement);
 		}
 	}
 
